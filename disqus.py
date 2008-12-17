eb60eba95281f3337c7f5bf6590370b7a9ffae43
Devin Naquin <dnaquin@gmail.com>
2008-12-17 15:57:42 -0600

    created empty project

---
+++ disqus.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+disqus.py
+
+Created by Devin Naquin on 2008-12-17.
+Copyright (c) 2008. All rights reserved.
+"""
+
+import sys
+import os
+import unittest
+
+
+class Api:
+	def __init__(self):
+		pass
+
+
+class ApiTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ timedcache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+timedcache.py
+
+Created by Devin Naquin on 2008-12-17.
+Copyright (c) 2008. All rights reserved.
+"""
+
+import sys
+import os
+import unittest
+
+
+class TimedCache:
+	def __init__(self):
+		pass
+
+
+class TimedCacheTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()

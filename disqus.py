fd566e87ce69de569552c99dce4ba8829cc63ad8
Devin Naquin <dnaquin@gmail.com>
2008-12-17 18:26:37 -0600

    key required decorators.

--- disqus.py
+++ disqus.py
@@ -7,15 +7,27 @@ Created by Devin Naquin on 2008-12-17.
 Copyright (c) 2008. All rights reserved.
 """
 
-import sys
-import os
-import unittest
+import unittest, warnings
+
+# TODO abstract into same method.
+# TODO conditionally warn if key not set. how to do with decorator?
+def user_key_required(f):
+	def wrapped(*args, **kwargs):
+		warnings.warn("method requires a user api key set in the Api class's constructor.")
+		return f(*args, **kwargs)
+	return wrapped
+		
+def forum_key_required(f):
+	def wrapped(*args, **kwargs):
+		warnings.warn("method requires a forum api key set in the Api class's constructor.")
+		return f(*args, **kwargs)
+	return wrapped
 
 
 class Api(object):
 
 
-	def __init__(self):
+	def __init__(self, forum_key=None, user_key=None):
 		pass
 
 

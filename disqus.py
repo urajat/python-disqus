37edce709a1f9a21fe0d07e7fdf0c012bcffbb55
Devin Naquin <dnaquin@gmail.com>
2008-12-18 23:07:21 -0600

    stubbed.

--- disqus.py
+++ disqus.py
@@ -9,6 +9,8 @@ Copyright (c) 2008. All rights reserved.
 
 import unittest, warnings
 
+import simplejson
+
 
 class Api(object):
 
@@ -27,13 +29,91 @@ class Api(object):
 	forum_key_required	= key_required('forum')
 
 
-	def __init__(self, forum_key=None, user_key=None):
-		self.forum_key = forum_key
-		self.user_key = user_key
+	def __init__(self, forum_key=None, user_key=None, disqus_fetcher=None):
+		self.forum_key 	= forum_key
+		self.user_key	= user_key
 
-	@user_key_required
-	def user_method(self):
-		print 'user_method'
+		# TODO check for none and insert default
+		self.disqus_fetcher = None
+
+
+class Forum(object):
+
+
+	def __init__(self, id, shortname, name):
+		self.id			= id
+		self.shortname	= shortname
+		self.name		= name
+
+
+class Thread(object):
+
+
+	def __init__(self, id, forum_id, slug, title, created_at, allow_comments,
+			url, identifier):
+		self.id				= id
+		self.forum_id		= forum_id
+		self.slug			= slug
+		self.title			= title
+		self.created_at		= created_at
+		self.allow_comments	= allow_comments
+		self.url			= url
+		self.identifier		= identifier
+
+
+class Post(object):
+
+
+	def __init__(self, id, forum_id, thread_id, created_at, message,
+			parent_post_id, shown, is_anonymous, anonymous_author, author):
+		self.id					= id
+		self.forum_id			= forum_id
+		self.thread_id			= thread_id
+		self.created_at			= created_at
+		self.message			= message
+		self.parent_post_id		= parent_post_id
+		self.shown				= shown
+		self.is_anonymous		= is_anonymous
+		# TODO author depends on is_anonymous.
+		# TODO author is compound data type
+		self.anonymous_author	= anonymous_author
+		self.author				= author
+
+
+class DisqusFetcher(object):
+
+
+	def __init__(self):
+		pass
+
+	# get methods
+	def get_forum_list(self):
+		pass
+
+	def get_forum_api_key(self):
+		pass
+
+	def get_thread_list(self):
+		pass
+
+	def get_thread_by_url(self):
+		pass
+
+	def get_thread_posts(self):
+		pass
+
+	def get_num_posts(self):
+		pass
+
+	# post methods
+	def create_post(self):
+		pass
+
+	def thread_by_identifier(self):
+		pass
+
+	def update_thread(self):
+		pass
 
 
 class ApiTests(unittest.TestCase):

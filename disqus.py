94c279de64f67fb5661ba32eb3582aee9c5cdd33
Rajat Upadhyaya <urajat@gmail.com>
2009-01-19 23:31:20 +0530

    Modifications to disqus.py:  *) Python 2.6 dependency for json  *) All documented DISQUS API methods implemented  *) Added examples of API use

--- disqus.py
+++ disqus.py
@@ -1,171 +1,202 @@
-#!/usr/bin/env python
-# encoding: utf-8
 """
-disqus.py
+    disqus.py
 
-Created by Devin Naquin on 2008-12-17.
-Copyright (c) 2008. All rights reserved.
+    A Python interface to DISQUS API
+
+    Copyright (c) 2009 Rajat Upadhyaya
+    Partly based on Devin Naquin's 'python-disqus' at http://github.com/devin/python-disqus
 """
 
-import unittest, warnings
+import sys
+import urllib, urllib2
 
-import simplejson as json
+# Python 2.6 or greater
+import json
 
-
-class Api(object):
-	"""
-	TODO write
-	"""
-
-	def key_required(key):
-		def decorator(f):
-			def wrapped(*args, **kwargs):
-				self = args[0]
-				if not getattr(self, '%s_key' % key):					
-					warnings.warn("method requires a %s api key set in the Api class's constructor." % key)
-				return f(*args, **kwargs)
-			return wrapped
-		return decorator
-
-	user_key_required 	= key_required('user')
-	forum_key_required	= key_required('forum')
+from BaseHTTPServer import BaseHTTPRequestHandler
 
 
-	def __init__(self, forum_key=None, user_key=None, disqus_fetcher=None):
-		self.forum_key 	= forum_key
-		self.user_key	= user_key
+class ApiError(Exception):
+    """ Error raised by a Disqus API call """
 
-		# TODO check for none and insert default
-		self.disqus_fetcher = None
+    def __init__(self, code, message):
+        self.code, self.mesg = code, message
 
-
-class Forum(object):
-
-
-	def __init__(self, id, shortname, name):
-		self.id			= id
-		self.shortname	= shortname
-		self.name		= name
+    def __str__(self):
+        return '%s (%s)' % (self.code, self.mesg)
 
 
-class Thread(object):
+class Api:
+    """ 
+        Encapsulates Disqus API methods 
+	API docs at http://disqus.com/docs/api/
+    """
+
+    # Base path for API methods
+    URL = 'http://disqus.com/api/'
+
+    def __init__(self, user_key):
+        self.user_key = user_key
+
+    def set_forum_key(self, forum_key):
+        self.forum_key = forum_key
+
+    def invoke(self, name, args={}, type='GET'):
+        method_url = self.URL + name + '/'
+
+	if name in ['get_forum_list', 'get_forum_api_key']:
+	    args['user_api_key'] = self.user_key
+	else:
+	    args['forum_api_key'] = self.forum_key
+       	data = urllib.urlencode(args)
+
+        try:
+            if type == 'GET':
+                response = urllib2.urlopen(method_url + '?' + data)
+            else:
+                req = urllib2.Request(method_url, data)
+                response = urllib2.urlopen(req)
+        except IOError, e:
+            if hasattr(e, 'reason'):
+                print 'URL Error: %s' % e.reason
+            elif hasattr(e, 'code'):
+                print 'HTTP Error %d: %s' % (e.code, BaseHTTPRequestHandler.responses[e.code])
+            else:
+	        print 'IO Error: %s' % e
+            return False
+
+        r = json.load(response)
+	if r['succeeded']:
+	    return r['message']
+	
+	raise ApiError(r['code'], r['message'])
+
+    # GET methods
+    def get_forum_list(self):
+        response = self.invoke('get_forum_list')
+
+	# Assumes single forum
+	return response[0]
+
+    def get_forum_api_key(self, forum_id):
+        return self.invoke('get_forum_api_key', { 'forum_id': forum_id })
+
+    def get_thread_list(self):
+	return self.invoke('get_thread_list')
+
+    def get_num_posts(self, thread_list):
+	return self.invoke('get_num_posts', { 'thread_ids': ','.join(thread_list) })
+
+    def get_thread_by_url(self, url):
+	return self.invoke('get_thread_by_url', { 'url': url })
+
+    def get_thread_posts(self, thread_id):
+	return self.invoke('get_thread_posts', { 'thread_id': thread_id })
+
+    # POST methods
+    def create_post(self, attributes):
+        return self.invoke('create_post', attributes, type='POST')
+
+    def thread_by_identifier(self, identifier, title=None):
+        return self.invoke('thread_by_identifier',
+                           {'identifier': identifier, 'title': title },
+                           type='POST')
+
+    def update_thread(self, thread_id, attributes={}):
+        attributes['thread_id'] = thread_id
+        return self.invoke('update_thread', attributes, type='POST')
 
 
-	def __init__(self, id, forum_id, slug, title, created_at, allow_comments,
-			url, identifier):
-		self.id				= id
-		self.forum_id		= forum_id
-		self.slug			= slug
-		self.title			= title
-		self.created_at		= created_at
-		self.allow_comments	= allow_comments
-		self.url			= url
-		self.identifier		= identifier
+class Forum:
+    FIELDS = ['id', 'shortname', 'name']
+    
+    def __init__(self, attributes):
+        for k, v in attributes.iteritems():
+	    if k in self.FIELDS:
+                setattr(self, k, v)
 
 
-class Post(object):
+class Thread:
+    FIELDS = ['id', 'forum', 'slug', 'title', 'created_at', 'allow_comments', 'url', 'identifier']
+    
+    def __init__(self, attributes):
+        for k, v in attributes.iteritems():
+	    if k in self.FIELDS:
+                setattr(self, k, v)
 
 
-	def __init__(self, id, forum_id, thread_id, created_at, message,
-			parent_post_id, shown, is_anonymous, anonymous_author, author):
-		self.id					= id
-		self.forum_id			= forum_id
-		self.thread_id			= thread_id
-		self.created_at			= created_at
-		self.message			= message
-		self.parent_post_id		= parent_post_id
-		self.shown				= shown
-		self.is_anonymous		= is_anonymous
-		# TODO author depends on is_anonymous.
-		# TODO author is compound data type
-		self.anonymous_author	= anonymous_author
-		self.author				= author
-
-
-class DisqusFetcher(object):
-	"""
-	TODO write
-	returns JSON
-	"""
-
-
-	def __init__(self):
-		pass
-
-	# get methods
-	def get_forum_list(self):
-		pass
-
-	def get_forum_api_key(self, forum_id):
-		pass
-
-	def get_thread_list(self):
-		pass
-
-	def get_thread_by_url(self):
-		pass
-
-	def get_thread_posts(self):
-		pass
-
-	def get_num_posts(self):
-		pass
-
-	# post methods
-	def create_post(self):
-		pass
-
-	def thread_by_identifier(self):
-		pass
-
-	def update_thread(self):
-		pass
-
-
-class ApiTests(unittest.TestCase):
-
-
-	class MockFetcher(DisqusFetcher):
-
-		def __init__(self, forums, threads, posts):
-			self.forums		= forums
-			self.threads	= threads
-			self.posts		= posts
-
-		# get methods
-		def get_forum_list(self):
-			serializable = [forum.__dict__ for forum in self.forums]
-
-			return json.dumps(serializable)
-
-		def get_forum_api_key(self, forum_id):
-			pass
-
-		def get_thread_list(self):
-			pass
-
-		def get_thread_by_url(self):
-			pass
-
-		def get_thread_posts(self):
-			pass
-
-		def get_num_posts(self):
-			pass
-
-		# post methods
-		def create_post(self):
-			pass
-
-		def thread_by_identifier(self):
-			pass
-
-		def update_thread(self):
-			pass
-
-	def setUp(self):
-		pass
+class Post:
+    FIELDS = ['id', 'forum', 'thread', 'created_at', 'message', 'parent_post', 'shown', 'is_anonymous', 
+               'anonymous_author', 'author']
+    
+    def __init__(self, attributes):
+        for k, v in attributes.iteritems():
+	    if k in self.FIELDS:
+                setattr(self, k, v)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    if len(sys.argv) == 1:
+        print "ERROR: User key not supplied"
+        print "Usage: %s <user_key>" % sys.argv[0]
+        sys.exit(1)
+
+    api = Api(sys.argv[1])
+    f = api.get_forum_list()
+    print f
+    print "Forum id = " + f['id']
+#    f = Forum(t)
+
+    fk = api.get_forum_api_key(f['id'])
+    print "Forum key = " + fk
+    api.set_forum_key(fk)
+
+"""
+    Examples:
+    ---------
+#   Get all threads associated with the forum
+    threads = api.get_thread_list()
+    print json.dumps(threads, sort_keys=True, indent=4)
+#    threads = [Thread(t) for t in api.get_thread_list()]
+
+#   Get the number of posts for each thread whose id is specified
+    t = api.get_num_posts(['1234567', '7654321'])
+    print t
+
+#   Get a thread by its URL and update various fields
+#    t = api.get_thread_by_url('http://example.com/some/path/')
+    print t
+    t = api.update_thread(t['id'], 
+                          {
+			   'title'         : 'Some title here',
+			   'slug'          : 'some_slug_here',
+			   'url'           : 'http://example.com/path/',
+			   'allow_comments': 0                             # 0 or 1
+			  })
+    print t
+
+#   Get all posts with the given 'id'
+    t = api.get_thread_posts(t['id'])
+    print json.dumps(t, sort_keys=True, indent=4)
+#    posts = [Post(p) for p in api.get_thread_posts('1234567')]
+
+#   Attach a post as a reply to another post
+    m = {'author_name' : 'Me', 
+         'author_email': 'me@example.com',
+         'author_url'  : 'http://example.com/',
+         'ip_address'  : '192.168.1.1'}
+    t = api.get_thread_posts(t['id'])
+    parent = t[4]
+    th = t[1]
+    m['thread_id']   = th['thread']
+    m['message']     = th['message']
+    m['created_at']  = th['created_at']
+    m['parent_post'] = parent['id']
+    r = api.create_post(m)
+    print r
+
+#   Get a thread by its identifier or create a thread with the given identifier
+    t = api.thread_by_identifier('Some identifier here', 'Optional title here')
+    print t
+"""
+
